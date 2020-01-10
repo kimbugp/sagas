7f0928106dbdd115b45354af8d56182931baf1f2
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-01-10 10:27:56 +0300

    remove example

--- main.py
+++ main.py
@@ -2,47 +2,8 @@ import asyncio
 
 from sagas import SagaBuilder
 
-counter1 = 0
-counter2 = 0
-
-amount1 = 10
-amount2 = 15
-
-
-def count1():
-    print('count 1')
-    global counter1
-    counter1 += amount1
-
-
-def count2():
-    print('count 2')
-    global counter2
-    counter2 += amount2
-    raise Exception('some error happened')
-
-
-def counter1():
-    global counter1
-    counter1 -= amount1
-    print('compensating 1')
-
-
-def counter2():
-    global counter2
-    counter2 -= amount2
-    print('compensating 2')
-
-
-def another():
-    global counter2
-    counter2 += amount2
-
 
 saga_builder = SagaBuilder.create()
-
-saga_builder.add_step(count2, counter2)
-saga_builder.add_step(count1, counter1)
 
 saga = saga_builder.build()
 if __name__ == "__main__":
--- setup.sh
+++
@@ -1,5 +0,0 @@
-pip install --upgrade pip setuptools wheel
-python setup.py sdist bdist_wheel
-python3 -m twine check dist/*
-
-python -m twine upload dist/* --username $UNAME -p $PASSWORD --skip-existing
