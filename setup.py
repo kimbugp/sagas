7a2bb5717d35906c49ff01a558a15111fff404fb
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-01-10 10:36:06 +0300

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='sagas',
+    author="Kimbugwe Simon Peter",
+    author_email="kimbsimon2@gmail.com",
+    setup_requires=['pbr'],
+    pbr=True,
+)
