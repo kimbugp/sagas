d978b16ca96cff50e91bb2a692f061bf0508e07f
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-11 13:51:48 +0300

    add flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import asyncio
+import pytest
 
 
+@pytest.fixture(scope='module')
 def event_loop():
+    """Fixture for the async event loop
+
+    Yields:
+        event loop
+    """
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
